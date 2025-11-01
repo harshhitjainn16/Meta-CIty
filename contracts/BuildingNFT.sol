d4cfbca60b0ad090d52e228503a14ddf1c185609
harshhitjainn16 <harshitjain1605@gmail.com>
2025-11-01 09:52:03 +0530

    fix

--- contracts/BuildingNFT.sol
+++ contracts/BuildingNFT.sol
@@ -4,18 +4,25 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
  * @title BuildingNFT
  * @dev NFT representing buildings in MetaCity
  */
 contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-    
-    enum BuildingType { Residential, Commercial, Industrial, Park, SolarFarm, Hospital, School, RecyclingCenter }
-    
+    uint256 private _tokenIdCounter;
+
+    enum BuildingType {
+        Residential,
+        Commercial,
+        Industrial,
+        Park,
+        SolarFarm,
+        Hospital,
+        School,
+        RecyclingCenter
+    }
+
     struct Building {
         BuildingType buildingType;
         uint256 level;
@@ -26,10 +33,10 @@ contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
         uint256 x;
         uint256 y;
     }
-    
+
     mapping(uint256 => Building) public buildings;
     mapping(address => bool) public authorizedMinters;
-    
+
     event BuildingMinted(
         uint256 indexed tokenId,
         address indexed owner,
@@ -38,18 +45,27 @@ contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
         uint256 y
     );
     event BuildingUpgraded(uint256 indexed tokenId, uint256 newLevel);
-    
-    constructor() ERC721("MetaCity Building", "MCBUILDING") Ownable(msg.sender) {}
-    
+
+    constructor()
+        ERC721("MetaCity Building", "MCBUILDING")
+        Ownable(msg.sender)
+    {}
+
     modifier onlyAuthorized() {
-        require(authorizedMinters[msg.sender] || msg.sender == owner(), "Not authorized");
+        require(
+            authorizedMinters[msg.sender] || msg.sender == owner(),
+            "Not authorized"
+        );
         _;
     }
-    
-    function setAuthorizedMinter(address minter, bool authorized) external onlyOwner {
+
+    function setAuthorizedMinter(
+        address minter,
+        bool authorized
+    ) external onlyOwner {
         authorizedMinters[minter] = authorized;
     }
-    
+
     function mintBuilding(
         address to,
         BuildingType buildingType,
@@ -57,15 +73,17 @@ contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
         uint256 y,
         string memory uri
     ) external onlyAuthorized returns (uint256) {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        
-        uint256 sustainabilityScore = calculateInitialSustainability(buildingType);
+
+        uint256 sustainabilityScore = calculateInitialSustainability(
+            buildingType
+        );
         uint256 economicValue = calculateInitialEconomicValue(buildingType);
-        
+
         buildings[tokenId] = Building({
             buildingType: buildingType,
             level: 1,
@@ -76,23 +94,25 @@ contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
             x: x,
             y: y
         });
-        
+
         emit BuildingMinted(tokenId, to, buildingType, x, y);
         return tokenId;
     }
-    
+
     function upgradeBuilding(uint256 tokenId) external {
         require(ownerOf(tokenId) == msg.sender, "Not building owner");
-        
+
         Building storage building = buildings[tokenId];
         building.level += 1;
         building.sustainabilityScore += 10;
         building.economicValue += 100;
-        
+
         emit BuildingUpgraded(tokenId, building.level);
     }
-    
-    function calculateInitialSustainability(BuildingType buildingType) internal pure returns (uint256) {
+
+    function calculateInitialSustainability(
+        BuildingType buildingType
+    ) internal pure returns (uint256) {
         if (buildingType == BuildingType.Park) return 100;
         if (buildingType == BuildingType.SolarFarm) return 95;
         if (buildingType == BuildingType.RecyclingCenter) return 90;
@@ -102,8 +122,10 @@ contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
         if (buildingType == BuildingType.Commercial) return 40;
         return 30; // Industrial
     }
-    
-    function calculateInitialEconomicValue(BuildingType buildingType) internal pure returns (uint256) {
+
+    function calculateInitialEconomicValue(
+        BuildingType buildingType
+    ) internal pure returns (uint256) {
         if (buildingType == BuildingType.Commercial) return 1000;
         if (buildingType == BuildingType.Industrial) return 900;
         if (buildingType == BuildingType.Residential) return 500;
@@ -113,22 +135,16 @@ contract BuildingNFT is ERC721, ERC721URIStorage, Ownable {
         if (buildingType == BuildingType.RecyclingCenter) return 550;
         return 400; // Park
     }
-    
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
-    
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
--- contracts/CityDAO.sol
+++ contracts/CityDAO.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./MetaCityToken.sol";
 import "./BuildingNFT.sol";
 
@@ -13,7 +13,7 @@ import "./BuildingNFT.sol";
 contract CityDAO is Ownable, ReentrancyGuard {
     MetaCityToken public token;
     BuildingNFT public buildingNFT;
-    
+
     struct Proposal {
         uint256 id;
         address proposer;
@@ -32,21 +32,21 @@ contract CityDAO is Ownable, ReentrancyGuard {
         mapping(address => bool) hasVoted;
         mapping(address => uint256) voteAmount;
     }
-    
+
     struct Treasury {
         uint256 balance;
         uint256 totalDeposits;
         uint256 totalWithdrawals;
     }
-    
+
     mapping(uint256 => Proposal) public proposals;
     uint256 public proposalCount;
     uint256 public votingPeriod = 3 days;
-    uint256 public proposalThreshold = 100 * 10**18; // 100 tokens to propose
+    uint256 public proposalThreshold = 100 * 10 ** 18; // 100 tokens to propose
     uint256 public quorumPercentage = 10; // 10% of total supply
-    
+
     Treasury public treasury;
-    
+
     event ProposalCreated(
         uint256 indexed proposalId,
         address indexed proposer,
@@ -62,15 +62,12 @@ contract CityDAO is Ownable, ReentrancyGuard {
     event ProposalExecuted(uint256 indexed proposalId, bool passed);
     event TreasuryDeposit(address indexed from, uint256 amount);
     event TreasuryWithdrawal(address indexed to, uint256 amount);
-    
-    constructor(
-        address _token,
-        address _buildingNFT
-    ) Ownable(msg.sender) {
+
+    constructor(address _token, address _buildingNFT) Ownable(msg.sender) {
         token = MetaCityToken(_token);
         buildingNFT = BuildingNFT(_buildingNFT);
     }
-    
+
     function createProposal(
         string memory title,
         string memory description,
@@ -83,11 +80,14 @@ contract CityDAO is Ownable, ReentrancyGuard {
             token.balanceOf(msg.sender) >= proposalThreshold,
             "Insufficient tokens to propose"
         );
-        require(fundingRequired <= treasury.balance, "Insufficient treasury funds");
-        
+        require(
+            fundingRequired <= treasury.balance,
+            "Insufficient treasury funds"
+        );
+
         uint256 proposalId = proposalCount++;
         Proposal storage proposal = proposals[proposalId];
-        
+
         proposal.id = proposalId;
         proposal.proposer = msg.sender;
         proposal.title = title;
@@ -98,42 +98,49 @@ contract CityDAO is Ownable, ReentrancyGuard {
         proposal.fundingRequired = fundingRequired;
         proposal.startTime = block.timestamp;
         proposal.endTime = block.timestamp + votingPeriod;
-        
+
         emit ProposalCreated(proposalId, msg.sender, title, fundingRequired);
         return proposalId;
     }
-    
-    function vote(uint256 proposalId, bool support, uint256 amount) external nonReentrant {
+
+    function vote(
+        uint256 proposalId,
+        bool support,
+        uint256 amount
+    ) external nonReentrant {
         Proposal storage proposal = proposals[proposalId];
         require(block.timestamp < proposal.endTime, "Voting period ended");
         require(!proposal.hasVoted[msg.sender], "Already voted");
-        require(token.balanceOf(msg.sender) >= amount, "Insufficient token balance");
-        
+        require(
+            token.balanceOf(msg.sender) >= amount,
+            "Insufficient token balance"
+        );
+
         proposal.hasVoted[msg.sender] = true;
         proposal.voteAmount[msg.sender] = amount;
-        
+
         if (support) {
             proposal.votesFor += amount;
         } else {
             proposal.votesAgainst += amount;
         }
-        
+
         emit VoteCast(proposalId, msg.sender, support, amount);
     }
-    
+
     function executeProposal(uint256 proposalId) external nonReentrant {
         Proposal storage proposal = proposals[proposalId];
         require(block.timestamp >= proposal.endTime, "Voting period not ended");
         require(!proposal.executed, "Proposal already executed");
-        
+
         uint256 quorum = (token.totalSupply() * quorumPercentage) / 100;
         uint256 totalVotes = proposal.votesFor + proposal.votesAgainst;
-        
+
         proposal.executed = true;
-        
+
         if (totalVotes >= quorum && proposal.votesFor > proposal.votesAgainst) {
             proposal.passed = true;
-            
+
             // Mint building NFT
             buildingNFT.mintBuilding(
                 proposal.proposer,
@@ -142,40 +149,45 @@ contract CityDAO is Ownable, ReentrancyGuard {
                 proposal.y,
                 ""
             );
-            
+
             // Deduct from treasury
             treasury.balance -= proposal.fundingRequired;
             treasury.totalWithdrawals += proposal.fundingRequired;
         }
-        
+
         emit ProposalExecuted(proposalId, proposal.passed);
     }
-    
+
     function depositToTreasury() external payable {
         treasury.balance += msg.value;
         treasury.totalDeposits += msg.value;
         emit TreasuryDeposit(msg.sender, msg.value);
     }
-    
+
     function setVotingPeriod(uint256 _votingPeriod) external onlyOwner {
         votingPeriod = _votingPeriod;
     }
-    
+
     function setProposalThreshold(uint256 _threshold) external onlyOwner {
         proposalThreshold = _threshold;
     }
-    
+
     function setQuorumPercentage(uint256 _percentage) external onlyOwner {
         require(_percentage <= 100, "Invalid percentage");
         quorumPercentage = _percentage;
     }
-    
-    function getProposalVotes(uint256 proposalId) external view returns (uint256 votesFor, uint256 votesAgainst) {
+
+    function getProposalVotes(
+        uint256 proposalId
+    ) external view returns (uint256 votesFor, uint256 votesAgainst) {
         Proposal storage proposal = proposals[proposalId];
         return (proposal.votesFor, proposal.votesAgainst);
     }
-    
-    function hasVoted(uint256 proposalId, address voter) external view returns (bool) {
+
+    function hasVoted(
+        uint256 proposalId,
+        address voter
+    ) external view returns (bool) {
         return proposals[proposalId].hasVoted[voter];
     }
 }
